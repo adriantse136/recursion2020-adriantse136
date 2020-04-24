b478ff8e62f2e032be654853c9743d16245ec52b
Kevin Hayes <khayes@naperville203.org>
2020-04-24 08:01:21 -0500

    Addes starter code for Circle and Tri Fractal

---
+++ FractleStuff/Pain.java
@@ -0,0 +1,11 @@
+public class Pain
+{
+    public static void main(String[] args)
+    {
+        //CircleViewer viewer = new CircleViewer();
+        
+        SierpinskiViewer viewer = new SierpinskiViewer();
+
+        
+    }
+}
